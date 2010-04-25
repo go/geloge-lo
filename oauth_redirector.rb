eccb35445feb63db7710254fcba9ecc3882cfaf4
Yushi <yushi.nakai@gmail.com>
2010-04-25 10:49:42 +0900

    change misc settings

--- oauth_redirector.rb
+++ oauth_redirector.rb
@@ -4,7 +4,7 @@ require 'webrick'
 server = WEBrick::HTTPServer.new({:BindAddress => '127.0.0.1',
                                :Port => 80})
 
-server.mount_proc('/oauth/twitter/callback') {|req, res|
+server.mount_proc('/authcallbacktest') {|req, res|
   res.status = 302
   res['location'] = 'http://geloge-lo.appspot.com:8080' + req.unparsed_uri
   #res.body=req.unparsed_uri
