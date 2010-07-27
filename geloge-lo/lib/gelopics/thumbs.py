497bb07e4a73806a9ccd2b88c9543c1ebf52326a
Go Chiba <go.chiba@gmail.com>
2010-07-27 18:11:49 +0900

    fixed photo size when using TweetPhoto service

--- geloge-lo/lib/gelopics/thumbs.py
+++ geloge-lo/lib/gelopics/thumbs.py
@@ -2,7 +2,7 @@
 import re
 
 class Thumbs():
-    api_hosts = ["http://twitpic.com", "http://yfrog.com"]
+    api_hosts = ["http://twitpic.com", "http://yfrog.com", "http://tweetphoto.com"]
 
     @classmethod
     def genThumbs(self, text):
@@ -14,5 +14,8 @@ class Thumbs():
         elif q[1].search(text):
             text = text.replace(self.api_hosts[1], '<br><img src="http://yfrog.com')
             text += '.th.jpg">'
+        elif q[2].search(text):
+            text = text.replace(self.api_hosts[2], '<br><img width=150 src="http://TweetPhotoAPI.com/api/TPAPI.svc/imagefromurl?size=medium&url='+self.api_hosts[2])
+            text += '">'
 
         return text
